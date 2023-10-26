c2a81cccc95804f023d9a9568cbc5d87fa7a0bf8
manoharrupa <gunder@maveric-systems.com>
2023-10-26 18:02:16 +0530

    project fe with out node modules

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { signinServiceService } from 'src/app/services/singin-service.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+selectedMenuItem: any;
+selectedMenuIcon: any;
+user: string | null = sessionStorage.getItem('usertype'); 
+  showresetform = true;
+  constructor(private router:Router, private signinServiceService:signinServiceService){
+    if(this.user==='User'){
+      this.showresetform = false;
+      //console.log(this.showresetform);
+    }
+  }
+
+  getSessionUsername(): string | null {
+    return sessionStorage.getItem('userName');
+  }
+
+  ngOnInit() {
+    this.selectedMenuItem = localStorage.getItem('selectedMenuItem');
+    this.selectedMenuIcon = localStorage.getItem('selectedMenuIcon');
+    if(this.selectedMenuItem == null || this.selectedMenuIcon == null){
+      this.selectedMenuItem = "Customer Transactions Summary";
+      this.selectedMenuIcon = "bi bi-card-list";
+      localStorage.setItem('selectedMenuItem', this.selectedMenuItem);
+      localStorage.setItem('selectedMenuIcon', this.selectedMenuIcon);  
+    }
+  }
+
+  logoutUser() {
+    
+    const data = {
+      loginId: sessionStorage.getItem('loginId'),
+      userId: sessionStorage.getItem('userId'),
+    };
+    this.signinServiceService.logout(data).subscribe(
+      (response) => {
+        Swal.fire({
+          icon: 'success',
+          title: `Thank You... `,
+          text: 'Sign Out Successfull',
+        });
+        sessionStorage.clear();
+        localStorage.removeItem('selectedMenuItem');
+        localStorage.removeItem('selectedMenuIcon');
+        this.router.navigate(['/']);
+      },
+      (error) => {
+        console.error('Logout error:', error);
+        const errorMessage = error?.error?.message || 'An error occurred';
+            Swal.fire({
+              icon: 'error',
+              title: 'Sorry...',
+              text: `${errorMessage}`,
+            });
+      }
+    );
+  }
+
+
+  selectMenu(menuName: string, menuIcon: string) {
+      this.selectedMenuItem = menuName;
+      this.selectedMenuIcon = menuIcon;
+      localStorage.setItem('selectedMenuItem', this.selectedMenuItem);
+      localStorage.setItem('selectedMenuIcon', this.selectedMenuIcon);  
+  }
+
+}
